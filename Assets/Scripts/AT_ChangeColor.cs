31a94578507c7ed1128dc604aa1f287f1bde039a
Haojun Li <lihaojun@shernet.sheridancollege.ca>
2026-03-11 17:46:55 -0400

    finish smth in week9 class

---
+++ Assets/Scripts/AT_ChangeColor.cs
@@ -0,0 +1,48 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class AT_ChangeColor : ActionTask {
+
+		public Color color;	
+
+		private MeshRenderer meshRenderer;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+
+			meshRenderer = agent.GetComponent<MeshRenderer>();
+
+            return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+
+			meshRenderer.material.color = color;
+
+            EndAction(true);
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+			
+		}
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
---
+++ Assets/Scripts/AT_MoveTo.cs
@@ -0,0 +1,74 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class AT_MoveTo : ActionTask {
+
+
+
+		public BBParameter<Transform> locatoin;
+
+		public BBParameter<GameObject> target;
+
+		public float range;
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+
+
+
+            
+
+                
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+
+            Vector3 position = agent.transform.position;
+
+            Vector3 targetPosition = target.value.transform.position;
+
+            float Distance = Vector3.Distance(position, targetPosition);
+
+            if (Distance >= range)
+            {
+                agent.transform.position = Vector3.MoveTowards(position, locatoin.value.position, 1f * Time.deltaTime);
+
+                if (position == locatoin.value.position)
+                {
+                    EndAction(true);
+                }
+                else
+                {
+                    
+                }
+            }
+            else
+            {
+                EndAction(true);
+            }
+        }
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
