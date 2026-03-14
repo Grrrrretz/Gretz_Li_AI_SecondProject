0fd34a1fb02eb05b54e8357e40c58b0629c94e6d
HaoJun Li <lhj15733527022@gmail.com>
2026-03-14 17:23:12 -0400

    finish compare function
    finish compare function

--- Assets/Scripts/AT_MoveToxx.cs
+++ Assets/Scripts/AT_MoveToxx.cs
@@ -1,14 +1,19 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class AT_MoveToxx : ActionTask {
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit() {
+
+        public BBParameter<Transform> locatoin;
+
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
 			return null;
 		}
 
---
+++ Assets/Scripts/CT_ABigerThanB.cs
@@ -0,0 +1,55 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Conditions {
+
+	public class CT_ABigerThanB : ConditionTask {
+
+		public BBParameter<Transform> A;
+		public BBParameter<Transform> B;
+		float ASize;
+		float BSize;
+
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit(){
+
+			ASize = A.value.localScale.x * A.value.localScale.y * A.value.localScale.z;
+
+			BSize = B.value.localScale.x * B.value.localScale.y * B.value.localScale.z;
+
+
+
+
+            return null;
+		}
+
+		//Called whenever the condition gets enabled.
+		protected override void OnEnable() {
+			
+		}
+
+		//Called whenever the condition gets disabled.
+		protected override void OnDisable() {
+			
+		}
+
+		//Called once per frame while the condition is active.
+		//Return whether the condition is success or failure.
+		protected override bool OnCheck() {
+
+			if (ASize > BSize)
+			{
+                return true;
+			}
+			else
+			{
+				return false;
+            }
+			
+		}
+	}
+}
