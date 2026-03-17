85dcbe3deaca51e4b4b9098194b670cb3ff1dba7
HaoJun Li <lhj15733527022@gmail.com>
2026-03-16 22:33:24 -0400

    finish mat change

---
+++ Assets/Scripts/AT_ChangeMat.cs
@@ -0,0 +1,45 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class AT_ChangeMat : ActionTask {
+
+		public BBParameter<Renderer> target;
+
+        public BBParameter<Material> newMat;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+			
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+			target.value.material = newMat.value;
+            EndAction(true);
+
+
+        }
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
