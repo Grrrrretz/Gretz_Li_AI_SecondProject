b0b0c70e3ad039d6430a576a33743968c194a90d
HaoJun Li <lhj15733527022@gmail.com>
2026-03-31 20:14:32 -0400

    fix some bugs

--- Assets/Scripts/AT_Rotation.cs
+++ Assets/Scripts/AT_Rotation.cs
@@ -6,48 +6,57 @@ using UnityEngine;
 namespace NodeCanvas.Tasks.Actions {
 
 	public class AT_Rotation : ActionTask {
-		public BBParameter<Transform> target;
-		public BBParameter<float> Langle;
-		public BBParameter<float> Rangle;
+        public BBParameter<Transform> target;
+        public BBParameter<float> Langle = -30f;
+        public BBParameter<float> Rangle = 30f;
+        public BBParameter<float> rotateSpeed = 60f;
 
-        //Use for initialization. This is called only once in the lifetime of the task.
-        //Return null if init was successfull. Return an error string otherwise
-        protected override string OnInit() {
-			return null;
-		}
+        private float startY;
+        private bool turningRight = false;
 
-		//This is called once each time the task is enabled.
-		//Call EndAction() to mark the action as finished, either in success or failure.
-		//EndAction can be called from anywhere.
-		protected override void OnExecute() {
-			
-		}
-
-		//Called once per frame while the action is active.
-		protected override void OnUpdate() {
-			
-			target.value.Rotate(0, Langle.value * Time.deltaTime, 0);
-				if (target.value.rotation.y <= Langle.value)
-				{
-					target.value.rotation = Quaternion.Euler(0, Rangle.value, 0);
-					if (target.value.rotation.y >= Rangle.value)
-					{
-						EndAction(true);
-					}
-                    
-				}
-
-
+        protected override string OnInit()
+        {
+            return null;
         }
 
-		//Called when the task is disabled.
-		protected override void OnStop() {
-			
-		}
+        protected override void OnExecute()
+        {
+            startY = target.value.eulerAngles.y;
+            turningRight = false;
+        }
 
-		//Called when the task is paused.
-		protected override void OnPause() {
-			
-		}
-	}
+        protected override void OnUpdate()
+        {
+            float currentY = target.value.eulerAngles.y;
+
+            float relativeY = Mathf.DeltaAngle(startY, currentY);
+
+            if (!turningRight)
+            {
+                target.value.Rotate(0, -rotateSpeed.value * Time.deltaTime, 0);
+
+                if (relativeY <= Langle.value)
+                {
+                    turningRight = true;
+                }
+            }
+            else
+            {
+                target.value.Rotate(0, rotateSpeed.value * Time.deltaTime, 0);
+
+                if (relativeY >= Rangle.value)
+                {
+                    EndAction(true);
+                }
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected override void OnPause()
+        {
+        }
+    }
 }
